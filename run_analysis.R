8e33c6c885d6c9efcf64eddb641e3acdf08dc97d
Linghui Feng <42790423+MegicLF@users.noreply.github.com>
2019-12-23 13:05:33 -0600

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,19 @@
+## read the provided dataset in the working directory
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+
 ## Merges the training and the test sets to create one data set.
-x <- rbind(x_train, x_test)
+X <- rbind(X_train, X_test)
 y <- rbind(y_train, y_test)
 subject_data <- rbind(subject_train, subject_test)
-merged_data <- cbind(subject_data, y, x)
+merged_data <- cbind(subject_data, y, X)
 
 ## Extracts only the measurements on the mean and standard
 ## deviation for each measurement.
